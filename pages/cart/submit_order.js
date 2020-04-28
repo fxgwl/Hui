8d7ce3195d4035e4b3647e6dae941bd07dee93fe
15222871910@yeah.net <15222871910@yeah.net>
2020-04-28 16:19:27 +0800

    提交订单手机号码判断

--- pages/cart/submit_order.js
+++ pages/cart/submit_order.js
@@ -5,7 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    bg: "bg1"
+    bg: "bg1",
+    userName: '',
+    mobile: ''
+  },
+  userNameInput: function (e) {
+    this.setData({
+      userName: e.detail.value
+    })
+  },
+
+  mobileInput: function (e) {
+    this.setData({
+      mobile: e.detail.value
+    })
   },
   change: function () {
     this.setData({
@@ -13,9 +26,50 @@ Page({
     })
   }, 
   goNext: function () {
-    wx.navigateTo({
-      url: 'order_finish',
-    })
+    var userName = this.data.userName;
+    var mobile = this.data.mobile;
+    var phonetel = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
+    var name = /^[u4E00-u9FA5]+$/;
+    if (userName == '') {
+      wx.showToast({
+        title: '请输入用户名',
+        icon: 'succes',
+        duration: 1000,
+        mask: true
+      })
+
+      return false
+    } else if (mobile == '') {
+      wx.showToast({
+        title: '手机号不能为空',
+      })
+
+      return false
+    }
+    else if (mobile.length != 11) {
+      wx.showToast({
+        title: '手机号长度有误！',
+        icon: 'success',
+        duration: 1500
+      })
+      return false;
+    }
+
+    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
+    if (!myreg.test(mobile)) {
+      wx.showToast({
+        title: '手机号有误！',
+        icon: 'success',
+        duration: 1500
+      })
+      return false;
+    }else{
+      //return true;
+      wx.navigateTo({
+        url: 'order_finish',
+      })
+    }
+    
   },
   /**
    * 生命周期函数--监听页面加载
